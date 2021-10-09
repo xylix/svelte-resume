1e0167555f5be1b99963d6fe9faa2722f4cc29c2
Kerkko Pelttari <kerkko@robocorp.com>
2021-10-09 12:14:29 +0300

    Fix prettier and eslint configs

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  arrowParens: 'avoid',
+  singleQuote: false,
+  printWidth: 90,
+  plugins: ['prettier-plugin-svelte'],
+  semi: false,
+  svelteSortOrder: 'options-scripts-styles-markup',
+  svelteStrictMode: false,
+  svelteBracketNewLine: true,
+  svelteIndentScriptAndStyle: true,
+  trailingComma: 'all',
+  tabWidth: 4,
+}
